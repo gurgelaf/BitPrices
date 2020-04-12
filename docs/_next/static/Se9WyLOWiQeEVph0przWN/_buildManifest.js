e4d0790d74d2648b1666611b968537708c5bc696
gurgelaf <andnux0@gmail.com>
2020-04-11 21:35:43 -0300

    rm docs

--- docs/_next/static/Se9WyLOWiQeEVph0przWN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/about":[a,b,c],"/index":[a,b,c]}}("static\u002Fchunks\u002F3dfab838b07af3001d3b0e3c45ee586e8ffb31c4.bfbbc7baec1ba868590f.js","static\u002Fcss\u002Fstyles.4d84ab39.chunk.css","static\u002Fchunks\u002Fstyles.37da1e6c94fa5227f4d3.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
